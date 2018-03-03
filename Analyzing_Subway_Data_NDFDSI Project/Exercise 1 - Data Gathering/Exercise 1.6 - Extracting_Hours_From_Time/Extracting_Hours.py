4a48912109147ff8cc31b5bb465eb270f39ddf2e
Shubham Kandwal <36587738+shubham0297@users.noreply.github.com>
2018-03-03 12:40:58 +0530

    Update Extracting_Hours.py

--- Analyzing_Subway_Data_NDFDSI Project/Exercise 1 - Data Gathering/Exercise 1.6 - Extracting_Hours_From_Time/Extracting_Hours.py
+++ Analyzing_Subway_Data_NDFDSI Project/Exercise 1 - Data Gathering/Exercise 1.6 - Extracting_Hours_From_Time/Extracting_Hours.py
@@ -35,9 +35,14 @@ output_dataframe.to_csv("output_file.txt",index=None)					# Point 3
 '''
 KEY POINTS
 
-1.  NOTE ---->	'output.txt' is the output file of Exercise 1.5 . IT MUST BE PRESENT IN THE SAME FOLDER IN WHICH  this code(Extracting_Hours.py) is.
+1.  NOTE ----> 'output.txt' is the output file of Exercise 1.5 . IT MUST BE PRESENT IN THE SAME FOLDER IN WHICH  this code(Extracting_Hours.py) is.
 2.	Extractes just the hour part from TIMEn and converts it into integer.
-3.	Writing back the returned dataframe (output_dataframe) to "output_file.txt". 	This output.txt will contain only the hour part of the TIMEn column
+	NOTE ----> IF YOU WANT TO ADD A SEPARATE NEW COLUMN IN EXISTING DATAFRAME THEN WRITE THE FOLLOWING CODE THEN CHANGE THE FUNCTION DEFINITION AS
+	def time_to_hour(time):
+		time['Hour_only'] = time['TIMEn'].str.slice(-8,-6).astype(int)   					
+    		return time
+3.	Writing back the returned dataframe (output_dataframe) to "output_file.txt". 	The output.txt for our code will contain ONLY the hour part of the TIMEn column
+	If you want the output.txt to contain all the columns as and not just the hour part , change the function definition as given in KEY POINTS 2 
 	If we don't write index=None , an extra column will be added to output file showing row index 
-	Note ---->	 Now the 'output.txt' of Exercise 1.5 will be overwritten by the 'output.txt' created in point 3 .
+	Note ----> Now the 'output.txt' of Exercise 1.5 will be overwritten by the 'output.txt' created in point 3 .
 '''
